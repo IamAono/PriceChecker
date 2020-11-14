90088ff1d92e5d682a4fcfe5f300ddf00f4a5554
S.L <43483411+IamAono@users.noreply.github.com>
2020-11-14 11:10:00 -0800

    changed prices, worked on removing items

--- WebScrape.py
+++ WebScrape.py
@@ -9,48 +9,54 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless') # so that the web browser doesn't open
 driver_path = "C:\\Drivers\\chromedriver.exe"
 driver = webdriver.Chrome(executable_path = driver_path, chrome_options = options)
-prices = {} # key is the link, value is a tuple that looks like (xPath, price)
+prices = {} # key is the link, value is a list that has the xPath, price, and item name
 
 try:
-    prices = pickle.load(open("save.p","rb"))
+    prices = pickle.load(open("C:\\Github\\PriceChecker\\save.p","rb"))
     print("exists")
 except:
     pass
 
+
+def price_change():
+    changes = [] # holds the names of all items that have changed prices
+    for key in prices:
+        driver.get(prices[key][0])
+        try:
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, prices[key][1]))
+            )
+            price = driver.find_element_by_xpath(prices[key][1]).text
+            if check(key, price):
+                changes.append(key)
+                prices[key][2] = price # now we update the price
+        finally:
+            driver.quit()
+    if len(changes) != 0:
+        return True
+    return False
+
 # checks the current price, if it is different from the previous price, return True else False
 def check(link, current_price):
-    previous_price = prices[link][1]
+    previous_price = prices[link][2]
     if previous_price != current_price:
         return True
     return False
 
 # adds a link to the ditionary as a key with the xPath and price as its value
-def add(link, xPath):
+def add(name, link, xPath):
     driver.get(link)
     try:
         element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, xPath))
         )
         price = driver.find_element_by_xpath(xPath).text
-        prices[link] = (xPath, price)
+        prices[name] = [link, xPath, price]
     finally:
         driver.quit()
 
 if __name__ == "__main__":
-    change = False
-    # go through the items to see if any prices changed
-    for key in prices:
-        driver.get(key)
-        time.sleep(5) # give the page time to load
-        try:
-            element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, prices[key][0]))
-            )
-            price = driver.find_element_by_xpath(prices[key][0]).text
-            if check(key, price):
-                change = True
-        finally:
-            driver.quit()
+    change = price_change() # go through the items to see if any prices changed
     if change:
         print("Prices have changed for: ")
     else:
@@ -59,15 +65,26 @@ if __name__ == "__main__":
         print("1. add item\n2. remove item\n3. exit")
         r = input()
         if r == '1':
+            print("Name: ")
+            name = input()
             print("Link: ")
             link = input()
             print("xPath: ")
             xPath = input()
-            add(link, xPath)
-            pickle.dump(prices, open("save.p", "wb"))
+            add(name, link, xPath)
         elif r == '2':
-            pass
+            print("These are the items currently saved")
+            for key in prices:
+                print(key)
+            print("Enter in the name of the item you want to remove")
+            remove = input()
+            if prices.__contains__(remove):
+                prices.pop(remove)
+                print("Successfully removed")
+            else:
+                print("That is not a name in the dictionary")
         elif r == '3':
+            pickle.dump(prices, open("C:\\Github\\PriceChecker\\save.p", "wb"))
             break
         else:
             print("That is not a valid response.")
